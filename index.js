b3787eca80e0a1027d90d0c1e78fa1a95e97191b
Rakhi Sharma <rakhish1994@gmail.com>
2017-01-19 21:34:13 +0530

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+	if (!isDrawing) return; //will stop the the function from running when they are not moused down
+	console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousemdown', () => isDrawing = true);
+canvas.addEventListener('mousemup', () => isDrawing = false);
+canvas.addEventListener('mousemout', () => isDrawing = true);
