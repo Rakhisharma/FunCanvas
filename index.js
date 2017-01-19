b3a5359836ae3e15e536bd3fb87f3216926908aa
Rakhi Sharma <rakhish1994@gmail.com>
2017-01-19 22:08:41 +0530

    hue is awesome

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx strokeStyle = '#BADA55';
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 100;	
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true;
+
 
 function draw(e) {
 	if (!isDrawing) return; //will stop the the function from running when they are not moused down
@@ -27,7 +29,23 @@ function draw(e) {
 	lastX = e.offsetX;
 	lastY = e.offsetY;
 	//using ES6 we can write above two line in on. like this: [lastX, lastY]=[e.offsetX, e.offsetY]; this is callet restructring an array
+	
 	hue++;
+
+	if (hue > 360) {
+		hue = 0;
+	}
+
+	if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+		direction= !direction;
+	}
+
+	if (direction) {
+	  ctx.lineWidth++;	
+	} else {
+	ctx.lineWidth--;
+	}
+
 }
 
 canvas.addEventListener('mousemdown', () => {
@@ -38,3 +56,4 @@ canvas.addEventListener('mousemdown', () => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousemup', () => isDrawing = false);
 canvas.addEventListener('mousemout', () => isDrawing = true);
+
